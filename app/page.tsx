f1ea838512ef19db56ddf8b4622e6c23cc7f304d
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-07 15:01:30 +0530

    add readme file

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
     <div>
-      <h1>React Shadcn Template</h1>
+      <h1>Nextjs Template</h1>
       <ModeToggle />
     </div>
   );
